c1ae232742e039ad828e84fcc31278fb8b3ee5ad
lxykad <503215875@qq.com>
2016-08-07 22:45:04 +0800

    js

--- app/src/main/java/com/lxy/js/MainActivity.java
+++ app/src/main/java/com/lxy/js/MainActivity.java
@@ -3,17 +3,34 @@ package com.lxy.js;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView mWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        mWebView = (WebView) findViewById(R.id.web_view);
+        //启用支持javascript
+        WebSettings settings = mWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+
     }
 
     public void showJs(View view){
-
+        mWebView.loadUrl("http:\\www.baidu.com");
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
     }
 }
